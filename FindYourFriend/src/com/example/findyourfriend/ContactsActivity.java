5ed9fc1f7c3c56db50223697cb5e02064cf6781f
Sveta <svetazmiev@gmail.com>
2014-04-05 23:37:00 +0300

    Создание проекта
    Обязатаельно автор класса указывает своё имя вверху и оставляет полные
    комментарии к своей части, когда коммитите оставляйте галочки только на
    своём классе!

---
+++ FindYourFriend/src/com/example/findyourfriend/ContactsActivity.java
@@ -0,0 +1,13 @@
+package com.example.findyourfriend;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.contects);
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ FindYourFriend
 ==============
 
 with this project you can know your position
+
+Обязательно оставляйте полные комментарии к своему коммиту!!!
